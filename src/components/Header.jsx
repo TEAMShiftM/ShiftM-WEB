08b39e3b83fb322036d01b245b09aa7fce8e83c6
jiruen <kjr2699@naver.com>
2025-04-26 16:25:28 +0900

    feat(#1): Header, Button, Input 컴포넌트 구현

---
+++ src/components/Header.jsx
@@ -0,0 +1,67 @@
+import React from "react";
+import styled from "styled-components";
+import LOGO from "../assets/logo.png";
+import Button from "./Button.jsx";
+
+const Header = () => {
+    return (
+        <HeaderS>
+            <LogoS>
+                <img src={LOGO} alt="shiftM"/>
+            </LogoS>
+            <ButtonS>
+                <Button text={"로그인"} color={"blue"}/>
+            </ButtonS>
+        </HeaderS>
+    );
+};
+
+const HeaderS = styled.header`
+    width: 100%;
+    height: 70px;
+    background-color: rgba(255, 255, 255, 0.5);
+    display: flex;
+
+    top: 0;
+    left: 0;
+    right: 0;
+    position: fixed;
+    z-index: 1000;
+    align-items: center;
+    justify-content: space-between;
+
+    @media (max-width: 768px) {
+        height: 60px;
+    }
+`;
+
+const LogoS = styled.div`
+    cursor: pointer;
+
+    img {
+        height: 60px;
+        margin-left: 15px;
+
+        @media (max-width: 768px) {
+            margin-left: 6px;
+            height: 45px;
+        }
+    }
+`;
+
+const ButtonS = styled.div`
+    button {
+        font-size: 1rem;
+        margin-right: 15px;
+        width: 100px;
+
+        @media (max-width: 768px) {
+            margin-right: 7px;
+            font-size: 0.8rem;
+            width: 70px;
+            height: 35px;
+        }
+    }
+`;
+
+export default Header;
