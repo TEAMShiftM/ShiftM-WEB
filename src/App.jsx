aeca2559f6b93a3e830d7a5789706b0a2bda5efa
yeon2lee <md04254@naver.com>
2025-05-05 20:55:59 +0900

    ui(#3): 라우팅 및 필요한 라이브러리 추가

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,21 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
+import ShiftDetail from "./pages/ShiftDetail";
+import ShiftUpdate from "./pages/ShiftUpdate";
+import AdminShift from "./pages/AdminShift";
+import AdminAfterCheckin from "./pages/AdminAfterCheckin";
+import AdminCompanyCreate from "./pages/AdminCompanyCreate";
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/shifts" element={<Home />} />
+        <Route path="/shift" element={<Home />} />
+        <Route path="/shift/detail" element={<ShiftDetail />} />
+        <Route path="/admin/shift/update" element={<ShiftUpdate />} />
+        <Route path="/admin/shift" element={<AdminShift />} />
+        <Route path="/admin/shift/after-checkin" element={<AdminAfterCheckin />} />
+        <Route path="/admin/company/create" element={<AdminCompanyCreate />} />
       </Routes>
     </Router>
   );
